2ca71d7a0784a073ea75867bcb15d47927cf0a35
kei.sugawa <keisugawa.23@gmail.com>
2019-08-18 23:51:52 +0900

    Add getAllTasks and Fixed related files, replaced DBUtil

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -4,13 +4,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class DBUtils {
+public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
-    public static EntityManager CreateEntityManager() {
+    public static EntityManager createEntityManager() {
         return _getEntityManagerFactory().createEntityManager();
-
     }
 
     private static EntityManagerFactory _getEntityManagerFactory() {
